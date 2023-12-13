621b67eba8082a3086fea532ead538bf5d69d39c
Dan Bolton <danjabolton@yahoo.co.uk>
2023-12-13 12:32:58 +0000

    Added a Chirp Factory and updated the database seeder

---
+++ database/factories/ChirpFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Chirp>
+ */
+class ChirpFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'message' => fake()->sentence(),
+            'user_id' => fake()->numberBetween(1,20),
+        ];
+    }
+}
